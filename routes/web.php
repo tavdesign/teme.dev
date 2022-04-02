e07bd445004a41ad4a70d9a8a7fe32ff99a952a0
tavdesign <tav.design@gmail.com>
2022-04-02 11:14:53 +0300

    adding doompdf

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\PDFController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('generate-pdf', [PDFController::class, 'generatePDF']);
 
 Auth::routes();
 
